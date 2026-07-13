ca373d2744e87766f45417c7e8fbf38ced928162
Rohanxx10 <rohanrai7217@gmail.com>
2026-07-14 05:07:49 +0530

    new

--- src_backend/src/routes/skills.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { searchSkills, createSkill } = require('../controllers/skillController')
-
-router.get('/',  searchSkills)
-router.post('/', createSkill)
-
-module.exports = router
